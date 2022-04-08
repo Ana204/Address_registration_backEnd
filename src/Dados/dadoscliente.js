2a1fdf59c008d39ddeb850c0183cc0cf0a2beae3
ana204 <anagomes.lucia@hotmail.com>
2022-04-07 21:41:30 -0300

    validating the request fields

--- src/Dados/dadoscliente.js
+++ src/Dados/dadoscliente.js
@@ -1,9 +1,12 @@
 let dados_clientes = []
 
 module.exports = {
+    //func para salvar os dados no array
     salvarDadosCliente(dados){
         dados_clientes.push(dados)
     }, 
+    
+    //func para devolver os dados do array
     pegarDadosCliente(){
         return dados_clientes
     }
--- src/controller/formscontroller.js
+++ src/controller/formscontroller.js
@@ -3,8 +3,22 @@ const db = require("../Dados/dadoscliente")
 exports.post = (req , res) =>{
 
     var dados_cliente = req.body
-   
-    var dados ={
+
+    if(dados_cliente.Nome == '' || dados_cliente.Nome == undefined || dados_cliente.Nome == null){
+       return res.send("Preencha o campo nome")
+    } 
+    else if(dados_cliente.Email == '' || dados_cliente.Email == undefined || dados_cliente.Email == null){
+        return res.send("Prencha o campo email")
+    }
+    else if(dados_cliente.Telefone == '' || dados_cliente.Telefone == undefined || dados_cliente == null){
+        return res.send("Preencha o campo telefone")
+    }
+    else if (dados_cliente.Cep == '' || dados_cliente.Cep == undefined || dados_cliente == null) {
+        return res.send("Preencha o campo cep")
+    } 
+    else {
+        
+        var dados ={
             Id:dados_cliente.Id,
             Nome:dados_cliente.Nome,
             Email:dados_cliente.Email,
@@ -15,7 +29,15 @@ exports.post = (req , res) =>{
     
     db.salvarDadosCliente(dados)
     console.log(db.pegarDadosCliente())
+
+    res.send("Usuario cadastrado com sucesso !!")
+    }
+
+
+
 }
+
+
 
 
 
