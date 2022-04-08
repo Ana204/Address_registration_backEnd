69578ed70912bf8b47d7af27ce270494c7fbacd9
ana204 <anagomes.lucia@hotmail.com>
2022-04-07 21:56:39 -0300

    list of users

---
+++ src/controller/listar_cliente_controller.js
@@ -0,0 +1,7 @@
+const db = require("../Dados/dadoscliente")
+
+exports.get = (req, res ) => {
+
+    var list = db.pegarDadosCliente()
+    res.send(list)
+}
--- src/routers.js
+++ src/routers.js
@@ -2,8 +2,10 @@ const express = require("express")
 const router = express.Router()
 
 const forms_controller = require("./controller/formscontroller")
+const list_cliente_controller = require("./controller/listar_cliente_controller")
 
 router.post("/", forms_controller.post)
+router.get("/list", list_cliente_controller.get)
 
 
 module.exports = router
